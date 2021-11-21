210c1b3f99dca05fa808867108cebf30f17a5a68
maxjiang216 <76746545+maxjiang216@users.noreply.github.com>
2021-11-20 19:26:44 -0500

    Added governor elections, updated bias map rules

--- html/AED/result_compiler.py
+++ html/AED/result_compiler.py
@@ -1,6 +1,12 @@
 import os
 from utility import get_state_abbr
 
+def update_all(path="elections"):
+    '''Compiles a folder of folders
+    Update .html files, .js files'''
+
+    map_js_string(str(compile_all()))
+
 def compile_all(path="elections"):
     '''Compiles a folder of folders
     folder elections should be a folder of folders (US_president, etc.)'''
@@ -11,12 +17,16 @@ def compile_all(path="elections"):
     return out
 
 def compile_folder(path, state=False):
-    '''Compiles all files in folder path'''
+    '''Compiles all files in folder path
+    Updates .html file'''
 
     out = {}
+    years = []
     for filename in os.listdir(path):
         if filename.endswith(".txt"): # only consider .txt files (for safety)
             out[filename.split('.')[0]] = parse(path+'/'+filename, state)
+            years.append(filename.split('.')[0])
+    update_html(path.split('/')[-1]+".html", years)
     return out
 
 def parse(filename, state=False):
@@ -45,4 +55,29 @@ def parse(filename, state=False):
     out["_COLORS"] = color_dict
     return out
 
-print(compile_all())
+def map_js_string(data, path="map.js"):
+    s = ""
+    for line in open(path,'r'):
+        if "var elections = " in line:
+            s += "\tvar elections = {0}\n".format(str(data))
+        else:
+            s += line
+    open(path,'w').write(s)
+
+def update_html(path, years):
+    years.sort(reverse=True)
+    flag = False
+    s = ""
+    for line in open(path,'r'):
+        if "<select name=\"election-year\" id=\"election-year\">" in line:
+            flag = True
+            s += line
+            for year in years:
+                s += "\t\t<option value=\"{0}\">{0}</option>\n".format(year)
+        if "</select>" in line:
+            flag = False
+        if not flag:
+            s += line
+    open(path,'w').write(s)
+
+update_all()
