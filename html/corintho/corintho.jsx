a40cc775bd416e05289f45090cfe8a09aa534cab
maxjiang216 <76746545+maxjiang216@users.noreply.github.com>
2023-04-20 14:11:29 -0400

    working on legal moves

--- html/corintho/corintho.jsx
+++ html/corintho/corintho.jsx
@@ -5,7 +5,7 @@ const pieces = document.getElementsByClassName("piece");
 const pieceCounter = {
   capital: 0,
   column: 0,
-  base: 0,
+  zbase: 0,
 };
 
 function updateCounter(pieceType) {
@@ -26,13 +26,58 @@ function createBoard() {
         const pieceId = event.dataTransfer.getData("text");
         const piece = document.getElementById(pieceId);
         const pieceType = pieceId.split("-")[0];
-        if (pieceCounter[pieceType] < 4) {
-          const newPiece = piece.cloneNode(true);
-          newPiece.id = `${pieceId}-${Date.now()}`;
-          addDragEventToPiece(newPiece);
-          tile.appendChild(newPiece);
-          pieceCounter[pieceType]++;
-          updateCounter(pieceType);
+        const sourceTile = piece.parentElement;
+        // Check if the tile already has a piece of the same type
+        const existingPiece = Array.from(tile.children).find(
+          (child) => child.id.split("-")[0] === pieceType
+        );
+        if (existingPiece) {
+          return; // If there's already a piece of the same type, do nothing
+        }
+        if (pieceType === "zbase" && tile.children.length > 0) {
+          return; // Cannot place a base on top of another piece
+        }
+        if (tile.children.length > 0 && tile.children[0].id.split("-")[0] === "capital") {
+          return; // Cannot place a piece on top of a capital
+        }
+        if (pieceType === "capital" && tile.children.length == 1 && tile.children[0].id.split("-")[0] === "zbase") {
+          return; // Cannot place capital on top of base
+        }
+        if (sourceTile.classList.contains("piece-container")) {
+          if (pieceCounter[pieceType] < 4) {
+            const newPiece = piece.cloneNode(true);
+            newPiece.id = `${pieceId}-${Date.now()}`;
+            addDragEventToPiece(newPiece);
+            tile.appendChild(newPiece);
+            // Sort the pieces based on their pieceType
+            const sortedPieces = Array.from(tile.children).sort(
+              (a, b) => a.id.split("-")[0].localeCompare(b.id.split("-")[0])
+            );
+            // Clear the tile and re-append the sorted pieces
+            tile.innerHTML = "";
+            for (const piece of sortedPieces) {
+              tile.appendChild(piece);
+            }
+            pieceCounter[pieceType]++;
+            updateCounter(pieceType);
+          }
+        } else {
+          // If the source is another tile on the board
+          // Move all pieces from the source tile to the target tile
+          while (sourceTile.firstChild) {
+            const movedPiece = sourceTile.firstChild;
+            addDragEventToPiece(movedPiece);
+            tile.appendChild(movedPiece);
+            // Sort the pieces based on their pieceType
+            const sortedPieces = Array.from(tile.children).sort(
+              (a, b) => a.id.split("-")[0].localeCompare(b.id.split("-")[0])
+            );
+            // Clear the tile and re-append the sorted pieces
+            tile.innerHTML = "";
+            for (const piece of sortedPieces) {
+              tile.appendChild(piece);
+            }
+          }
         }
       };
 
