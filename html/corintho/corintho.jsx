b32433daf85fc4b298b260e194c7f479b8ba046c
maxjiang216 <76746545+maxjiang216@users.noreply.github.com>
2023-04-19 20:49:50 -0400

    drag and drop pieces

--- html/corintho/corintho.jsx
+++ html/corintho/corintho.jsx
@@ -1,6 +1,17 @@
-const boardElement = document.getElementById("board");
-const pieces = document.querySelectorAll(".piece");
-const pieceCounts = {};
+document.addEventListener("DOMContentLoaded", () => {
+  const boardElement = document.getElementById("board");
+const pieces = document.getElementsByClassName("piece");
+
+const pieceCounter = {
+  capital: 0,
+  column: 0,
+  base: 0,
+};
+
+function updateCounter(pieceType) {
+  const counterElement = document.getElementById(`counter-${pieceType}`);
+  counterElement.textContent = 4 - pieceCounter[pieceType];
+}
 
 function createBoard() {
   for (let row = 0; row < 4; row++) {
@@ -14,12 +25,15 @@ function createBoard() {
         event.preventDefault();
         const pieceId = event.dataTransfer.getData("text");
         const piece = document.getElementById(pieceId);
-        const newPiece = piece.cloneNode(true);
-        newPiece.id = `${pieceId}-${Date.now()}`;
-        addDragEventToPiece(newPiece);
-        tile.appendChild(newPiece);
         const pieceType = pieceId.split("-")[0];
-        updatePieceCount(pieceType, -1);
+        if (pieceCounter[pieceType] < 4) {
+          const newPiece = piece.cloneNode(true);
+          newPiece.id = `${pieceId}-${Date.now()}`;
+          addDragEventToPiece(newPiece);
+          tile.appendChild(newPiece);
+          pieceCounter[pieceType]++;
+          updateCounter(pieceType);
+        }
       };
 
       if ((row + col) % 2 === 0) {
@@ -36,50 +50,17 @@ function createBoard() {
 function addDragEventToPiece(piece) {
   piece.ondragstart = (event) => {
     event.dataTransfer.setData("text", event.target.id);
-    const pieceId = event.target.id;
-    const pieceType = pieceId.split("-")[0];
-    if (pieceCounts[pieceType] >= 4) {
-      event.preventDefault();
-    } else {
-      updatePieceCount(pieceType, 1);
-      pieceCounts[pieceType] += 1;
-    }
   };
 }
 
 function addDragEventsToPieces() {
   for (const piece of pieces) {
-    const pieceId = piece.id;
-    const pieceType = pieceId.split("-")[0];
-    pieceCounts[pieceType] = 0;
     addDragEventToPiece(piece);
-
-    // Add piece count element
-    const pieceCountElement = document.createElement("div");
-    pieceCountElement.classList.add("piece-count");
-    pieceCountElement.innerText = `${4 - pieceCounts[pieceType]}`;
-    pieceCountElement.style.backgroundColor = "#fff";
-    pieceCountElement.style.color = "#222";
-    pieceCountElement.style.borderRadius = "50%";
-    pieceCountElement.style.width = "20px";
-    pieceCountElement.style.height = "20px";
-    pieceCountElement.style.fontSize = "12px";
-    pieceCountElement.style.display = "flex";
-    pieceCountElement.style.alignItems = "center";
-    pieceCountElement.style.justifyContent = "center";
-    piece.style.position = "relative";
-    piece.appendChild(pieceCountElement);
-    pieceCountElement.style.position = "absolute";
-    pieceCountElement.style.bottom = "0";
-    pieceCountElement.style.right = "0";
   }
 }
 
-function updatePieceCount(pieceType, change) {
-  pieceCounts[pieceType] += change;
-  const pieceCountElement = document.querySelector(`.piece#${pieceType}`).nextSibling;
-  pieceCountElement.innerText = `${4 - pieceCounts[pieceType]} left`;
-}
 
 createBoard();
 addDragEventsToPieces();
+
+});
